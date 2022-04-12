6e0660177bbc0de600a215c3c00ab5af3ed51321
Niyozbk <niyozbekpulatov@gmail.com>
2022-04-12 18:24:23 +0500

    respepts rendered

--- dist/index.62406edb.js
+++ dist/index.62406edb.js
@@ -1,31 +1,26 @@
 const recipeContainer = document.querySelector('.recipe');
-import icons from '../img/icons.svg';
-
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
+const timeout = function(s) {
+    return new Promise(function(_, reject) {
+        setTimeout(function() {
+            reject(new Error(`Request took too long! Timeout after ${s} second`));
+        }, s * 1000);
+    });
 };
-
-async function renderRight() {
-  const id = window.location.hash.slice(1);
-  let dataJson = await fetch(
-    `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
-  );
-  let data = await dataJson.json();
-  let recipe = data.data.recipe;
-  renderHtml(recipe);
-  let recipeIngridient = recipe.ingredients;
-  recipeIngridient.forEach(element => {
-    reseptIngridient(element);
-  });
+async function renderRight(id) {
+    let dataJson = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886');
+    let data = await dataJson.json();
+    let recipe = data.data.recipe;
+    renderHtml(recipe);
+    let recipeIngridient = recipe.ingredients;
+    recipeIngridient.forEach((element)=>{
+        reseptIngridient(element);
+    });
+    console.log(data);
 }
-
+renderRight();
 function renderHtml(obj) {
-  recipeContainer.innerHTML = '';
-  let html = `<figure class="recipe__fig">
+    recipeContainer.innerHTML = '';
+    let html = `<figure class="recipe__fig">
   <img src="${obj.image_url}" alt="Tomato" class="recipe__img" />
   <h1 class="recipe__title">
     <span>${obj.title}</span>
@@ -35,14 +30,14 @@ function renderHtml(obj) {
 <div class="recipe__details">
   <div class="recipe__info">
     <svg class="recipe__info-icon">
-      <use href="${icons}#icon-clock"></use>
+      <use href="../src/img/icons.svg#icon-clock"></use>
     </svg>
     <span class="recipe__info-data recipe__info-data--minutes">${obj.cooking_time}</span>
     <span class="recipe__info-text">minutes</span>
   </div>
   <div class="recipe__info">
     <svg class="recipe__info-icon">
-      <use href="${icons}#icon-users"></use>
+      <use href="../src/img/icons.svg#icon-users"></use>
     </svg>
     <span class="recipe__info-data recipe__info-data--people">${obj.servings}</span>
     <span class="recipe__info-text">servings</span>
@@ -50,12 +45,12 @@ function renderHtml(obj) {
     <div class="recipe__info-buttons">
       <button class="btn--tiny btn--increase-servings">
         <svg>
-          <use href="${icons}#icon-minus-circle"></use>
+          <use href="../src/img/icons.svg#icon-minus-circle"></use>
         </svg>
       </button>
       <button class="btn--tiny btn--increase-servings">
         <svg>
-          <use href="${icons}#icon-plus-circle"></use>
+          <use href="../src/img/icons.svg#icon-plus-circle"></use>
         </svg>
       </button>
     </div>
@@ -63,12 +58,12 @@ function renderHtml(obj) {
 
   <div class="recipe__user-generated">
     <svg>
-      <use href="${icons}#icon-user"></use>
+      <use href="../src/img/icons.svg#icon-user"></use>
     </svg>
   </div>
   <button class="btn--round">
     <svg class="">
-      <use href="${icons}#icon-bookmark-fill"></use>
+      <use href="../src/img/icons.svg#icon-bookmark-fill"></use>
     </svg>
   </button>
 </div>
@@ -94,33 +89,26 @@ function renderHtml(obj) {
   >
     <span>Directions</span>
     <svg class="search__icon">
-      <use href="${icons}#icon-arrow-right"></use>
+      <use href="src/img/icons.svg#icon-arrow-right"></use>
     </svg>
   </a>
 </div>`;
-  recipeContainer.insertAdjacentHTML('afterbegin', html);
+    recipeContainer.insertAdjacentHTML('afterbegin', html);
 }
-
 function reseptIngridient(obj) {
-  let html = `
+    let html = `
   <li class="recipe__ingredient">
       <svg class="recipe__icon">
-        <use href="${icons}#icon-check"></use>
+        <use href="src/img/icons.svg#icon-check"></use>
       </svg>
-      <div class="recipe__quantity">${obj.quantity ? obj.quantity : ''}</div>
+      <div class="recipe__quantity">${obj.quantity}</div>
       <div class="recipe__description">
         <span class="recipe__unit">${obj.unit}</span>
         ${obj.description}
       </div>
     </li>`;
-  document
-    .querySelector('.recipe__ingredient-list')
-    .insertAdjacentHTML('afterbegin', html);
-}
+    document.querySelector('.recipe__ingredient-list').insertAdjacentHTML('afterbegin', html);
+} // https://forkify-api.herokuapp.com/v2
+ ///////////////////////////////////////
 
-['hashchange', 'load'].map(val => {
-  window.addEventListener(val, renderRight);
-});
-// https://forkify-api.herokuapp.com/v2
-
-///////////////////////////////////////
+//# sourceMappingURL=index.62406edb.js.map
